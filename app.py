c9e6a6a6f8402af492f2332a09c3af8771ecdb4b
Ahmedharris <ahmedharris@campusonweb.com>
2023-08-16 17:57:33 +0500

    dsf

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def update(sno):
         todo.description = description
         db.session.add(todo)
         db.session.commit()
-        return redirect ('/')
+        return "Record Updated"
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo = todo)
 
